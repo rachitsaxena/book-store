5444236bccf9519558ff82580c4733dc762310d6
rachitsaxena <rachit.id@gmail.com>
2016-09-29 20:20:37 +0530

    Eureka Client enabled

--- book-service/src/main/java/com/rachit/bookstore/service/book/BookServiceApplication.java
+++ book-service/src/main/java/com/rachit/bookstore/service/book/BookServiceApplication.java
@@ -2,8 +2,10 @@ package com.rachit.bookstore.service.book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class BookServiceApplication {
 
 	public static void main(String[] args) {
